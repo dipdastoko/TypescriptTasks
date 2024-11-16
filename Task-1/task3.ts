9e0b1196b9724e1ff4a495cc9882e800d3f4cdee
rpamgi <dipdastoko@gmail.com>
2024-11-16 23:53:48 +0600

    task 3 complete

---
+++ Task-1/task3.ts
@@ -0,0 +1,13 @@
+
+type Person = {
+    Name: string,
+    Address: string,
+    HairAndEyeColor: string,
+    IncomeAndExpenses: number,
+    Hobbies: string[],
+    FamilyMembers: string[],
+    Job: string,
+    skills: string[],
+    MaritalStatus: boolean,
+    Friends:string[]
+}
